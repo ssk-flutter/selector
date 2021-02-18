96e969ff5ab6165be14bd022705e3b6a622bf4bf
ssk <developer.kss@gmail.com>
2021-02-18 20:44:49 +0900

    androidOrElse, ...

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,36 +12,51 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('platform selector'),
-        ),
-        body: Column(
-          children: [
-            Center(child: Text('Running on: ${eachName()}\n')),
-            Center(
-                child: Text(
-                    'function on each platform: ${eachFunction()(1, 2)}\n')),
-          ],
-        ),
+        appBar: AppBar(title: Text('platform selector')),
+        body: Builder(builder: (context) => _buildBody(context)),
       ),
     );
   }
 
-  eachName() => selector(
-        android: 'Android',
-        ios: 'iOS',
-        windows: 'Windows',
-        linux: 'Linux',
-        mac: 'MacOS',
-        fuchsia: 'Fuchsia',
+  _buildBody(BuildContext context) => Column(
+        children: [
+          FlatButton(
+              child: Text('selector'),
+              onPressed: () => _showSnackBar(context, _selector())),
+          FlatButton(
+              child: Text('parameter as function'),
+              onPressed: () =>
+                  _showSnackBar(context, _functionParameter().toString())),
+          FlatButton(
+              child: Text('androidOrElse'),
+              onPressed: () => _showSnackBar(context, _androidOrElse())),
+          FlatButton(
+              child: Text('iosOrElse()'),
+              onPressed: () => _showSnackBar(context, _iosOrElse())),
+        ],
       );
 
-  eachFunction() => selector(
+  _showSnackBar(BuildContext context, String message) =>
+      Scaffold.of(context).showSnackBar(SnackBar(content: Text(message)));
+
+  _selector() => selector(
+      android: 'hello Android',
+      ios: 'hello iOS',
+      fuchsia: 'hello Fuchsia',
+      linux: 'hello Linux',
+      mac: 'hello MacOS',
+      windows: 'hello Windows');
+
+  _functionParameter() => selector(
         android: (a, b) => a + b,
         ios: (a, b) => a * b,
         windows: (a, b) => a / b,
         linux: (a, b) => pow(a, b),
         mac: (a, b) => sqrt(a) * b,
         fuchsia: (a, b) => a - b,
-      );
+      )(1, 2);
+
+  _androidOrElse() => androidOrElse('android-admob-id', 'ios-admob-id');
+
+  _iosOrElse() => iosOrElse('ios-admob-id', 'android-admob-id');
 }
--- lib/selector.dart
+++ lib/selector.dart
@@ -17,3 +17,33 @@ T selector<T>({
 
   throw 'Unidentified platform ${Platform.operatingSystem}';
 }
+
+T androidOrElse<T>(T android, T elsePlatform) {
+  if (Platform.isAndroid) return android;
+  return elsePlatform;
+}
+
+T iosOrElse<T>(T ios, T elsePlatform) {
+  if (Platform.isIOS) return ios;
+  return elsePlatform;
+}
+
+T fuchsiaOrElse<T>(T fuchsia, T elsePlatform) {
+  if (Platform.isFuchsia) return fuchsia;
+  return elsePlatform;
+}
+
+T linuxOrElse<T>(T linux, T elsePlatform) {
+  if (Platform.isLinux) return linux;
+  return elsePlatform;
+}
+
+T macOrElse<T>(T mac, T elsePlatform) {
+  if (Platform.isMacOS) return mac;
+  return elsePlatform;
+}
+
+T windowsOrElse<T>(T windows, T elsePlatform) {
+  if (Platform.isWindows) return windows;
+  return elsePlatform;
+}
